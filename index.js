4bdb177d468f265fefea15869fddd8697209eebf
Sidney <sidney.santos.zx@gmail.com>
2026-02-20 01:04:48 -0300

    feat: service, controller and routes users

--- index.js
+++ index.js
@@ -1,22 +1,10 @@
 import express from 'express'
+import userRouters from './src/routes/user.routes.js';
 const app = express()
-//Metodo => GET, POST, PUT/PATCH, DELETE
-//Name => / - sempre no plural
-// callback functions => Onde executamos o backend (logica, regra de negocio)
+
 
 app.use(express.json());
-const users = []
-
-app.post('/users', (req, res) => {
-  console.log(req)
-  const body = req.body
-  users.push(body)
-  res.status(201).send('Usuario criado com sucesso')
-});
-
-app.get('/users', (req, res) => {
-  res.send({users})
-})
+app.use(userRouters)
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000')
---
+++ src/controller/user.controller.js
@@ -0,0 +1,14 @@
+import userService from "../service/user.service.js";
+
+async function createUserController(req, res){
+    const newUser = req.body;
+
+    try{
+        const user = userService.createUserService(newUser);
+        res.status(201).send({user})
+    } catch {
+        return res.status(400).send(err.message);
+    }
+}
+
+export default {createUserController}
